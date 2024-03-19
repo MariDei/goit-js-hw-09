231a1b54001927910e2a6a5435d2dd173dd9ad16
Marianna Dei <d.marianna@icloud.com>
2024-03-19 22:38:20 +0200

    Added the SimpleLightbox library.

---
+++ src/js/1-gallery.js
@@ -0,0 +1,9 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+});
+console.log(SimpleLightbox);
