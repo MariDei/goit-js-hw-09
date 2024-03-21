9b5836033bda1320bbff35b4969e41d2244e943f
Marianna Dei <d.marianna@icloud.com>
2024-03-22 01:00:10 +0200

    Updated script to 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,72 +1,3 @@
-//*1
-// const STORAGE_KEY = 'feedback-form-state';
-
-// const form = document.querySelector('.feedback-form');
-// const textarea = form.querySelector('textarea');
-
-// form.addEventListener('submit', handleSubmit);
-// textarea.addEventListener('input', onTextareaInput);
-
-// populateTextarea();
-
-// function handleSubmit(event) {
-//   event.preventDefault();
-
-//   console.log('send');
-//   event.currentTarget.reset();
-//   localStorage.removeItem(STORAGE_KEY);
-// }
-
-// function onTextareaInput(event) {
-//   const message = event.target.value;
-//   localStorage.setItem(STORAGE_KEY, message);
-// }
-
-// function populateTextarea() {
-//   const savedMessage = localStorage.getItem(STORAGE_KEY);
-
-//   if (savedMessage) {
-//     textarea.value = savedMessage;
-//   }
-// }
-
-//*2
-// document.addEventListener('DOMContentLoaded', function () {
-//   const form = document.querySelector('.feedback-form');
-
-//   form.addEventListener('input', function (event) {
-//     const formData = {
-//       email: form.elements.email.value.trim(),
-//       message: form.elements.message.value.trim(),
-//     };
-//     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-//   });
-
-//   const storedFormData = JSON.parse(
-//     localStorage.getItem('feedback-form-state')
-//   );
-//   if (storedFormData) {
-//     form.elements.email.value = storedFormData.email;
-//     form.elements.message.value = storedFormData.message;
-//   }
-
-//   form.addEventListener('submit', function (event) {
-//     event.preventDefault();
-
-//     const email = form.elements.email.value.trim();
-//     const message = form.elements.message.value.trim();
-
-//     if (email && message) {
-//       console.log({ email, message });
-//       localStorage.removeItem('feedback-form-state');
-
-//       form.elements.email.value = '';
-//       form.elements.message.value = '';
-//     }
-//   });
-// });
-
-//*3
 const STORAGE_KEY = 'feedback-form-state';
 
 const form = document.querySelector('.feedback-form');
@@ -99,7 +30,7 @@ function onFormSubmit(event) {
   console.log({ email: email.value, message: message.value });
 
   if (email.value === '' || message.value === '') {
-    return alert(`Будь ласка, заповніть всі поля.`);
+    return alert(`Будь ласка, заповніть обидва поля.`);
   }
 
   localStorage.removeItem(STORAGE_KEY);
